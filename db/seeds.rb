d04f40526dfc6cd7b42064e53d4ef4b9d0c22e6a
Benjamin Oris Jr <bcoris@gmail.com>
2012-01-17 18:41:57 +0800

    Create user records.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['Ace', 'Benj', 'Mikong', 'Rico'].each do |name|
+  User.create(name: name)
+end
