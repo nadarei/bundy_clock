5c13ed413a632cd4ccb849acc762352f7d804346
Benjamin Oris Jr <bcoris@gmail.com>
2012-01-17 14:30:59 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BundyClock::Application.config.secret_token = '37831fd57bf21473f9932769bf4c6704777647cc63391039f7725008f3f48fe45731d2328f503db6d7bb60ce50587d328bcfac6d0f6e692078ed319bb8ff261d'
