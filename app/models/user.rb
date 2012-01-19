41494a09e3d121c66a81a640182d68ca6debbb72
Benjamin Oris Jr <bcoris@gmail.com>
2012-01-19 16:47:31 +0800

    current_time_log method for User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :time_log
+  has_many :time_logs
+
+  def current_time_log
+    self.time_logs.where('date = :date', { date: Date.today }).first
+  end
 end
