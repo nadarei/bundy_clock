9d603eeaafa136c00d2ffd2edf4d4a6e22238e7a
Benjamin Oris Jr <bcoris@gmail.com>
2012-01-19 16:45:15 +0800

    Define actions for TimeLogs controller.

--- app/controllers/time_logs_controller.rb
+++ app/controllers/time_logs_controller.rb
@@ -1,8 +1,17 @@
 class TimeLogsController < ApplicationController
-  def time_in
+  # TODO: use current_user when auth is implemented
+  # POST /time_in/user_id
+  def time_in 
+    @time_in = TimeLog.new(date: Date.today in: Time.now, user_id: user_id)
   end
 
+  # PUT /time_in/user_id/edit
+  def edit_in
+    @time_in = TimeLog.find(date: Date.today, user_id: user_id)
+
+  # PUT /time_out/user_id/edit
   def time_out
+    @time_out = TimeLog.find(date: Date.today, user_id: user_id)
   end
 
 end
