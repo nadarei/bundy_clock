7a4fa8b987e427851f3e3083e886def8f3bd2221
Benjamin Oris Jr <bcoris@gmail.com>
2012-01-20 18:21:03 +0800

    Create Comment model and make 1-1 association with TimeLog.

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,3 +1,4 @@
 class TimeLog < ActiveRecord::Base
   belongs_to :user
+  has_one :comment
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120118073127) do
+ActiveRecord::Schema.define(:version => 20120120095603) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "time_logs", :force => true do |t|
     t.date     "date"
