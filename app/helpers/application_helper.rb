e36c58c72fd10b9bd5b095e1dba8452f3ac7c55d
Benjamin Oris Jr <bcoris@gmail.com>
2012-01-27 16:56:46 +0800

    Make date and time zone-aware.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
   def short_time(time)
-    time.localtime.strftime("%H:%M")
+    # config.time_zone set to 'Beijing'. No 'Manila' yet in Rails. Boo!
+    time.in_time_zone.strftime("%H:%M")
   end
 
-  def long_date(date)
-    date.strftime("%A, %B %e, %Y")
+  def long_date(time)
+    time.in_time_zone.strftime("%A, %B %e, %Y")
   end
 end
