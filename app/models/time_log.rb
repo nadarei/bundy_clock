cb9339269866d5dabbdb455f65db3380d44d0a7a
Benjamin Oris Jr <bcoris@gmail.com>
2012-02-01 18:27:14 +0800

    Add hours to user report and classes to td tags.

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,4 +1,14 @@
 class TimeLog < ActiveRecord::Base
   belongs_to :user
   has_one :comment
+  #validates :time_in, { :format => %r, :on => :update }
+  #validates :time_out, { :format => %r, :on => :update }
+  #validates :time_out_is_after_time_in, on: :update
+  #validates :time_in_is_before_time_out, on: :update
+
+  def hours
+    if out
+      (out - self.in) / 60 / 60
+    end
+  end
 end
