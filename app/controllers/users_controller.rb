de52f8429a2c6e5f77b8eb5ff966b6a8c64eb9d7
Benjamin Oris Jr <bcoris@gmail.com>
2012-02-02 17:39:48 +0800

    Add Total Hours in user report.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    @period = UserPeriod.new(@user.time_logs)
+
+    @total_hours = @period.hours
   end
 end
