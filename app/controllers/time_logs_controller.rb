028327da45f36b36c3a8e6abb9d337d8d1b2b198
Benjamin Oris Jr <bcoris@gmail.com>
2012-02-10 16:10:19 +0800

    Use current_user to simplify code.

--- app/controllers/time_logs_controller.rb
+++ app/controllers/time_logs_controller.rb
@@ -1,24 +1,21 @@
 class TimeLogsController < ApplicationController
   # POST /time_logs/time_in
   def time_in 
-    # TODO: use auth later to pass user_id
-    @user = User.find(params[:user_id])
-    @user.time_logs.create(date: Date.today, in: Time.now)
+    current_user.time_logs.create(date: Date.today, in: Time.now)
 
     redirect_to root_url
   end
 
   # PUT /time_logs/time_out
   def time_out
-    current_time_log = TimeLog.find(params[:time_log_id])
-    current_time_log.update_attributes out: Time.now
+    current_user.current_time_log.update_attributes out: Time.now
 
     redirect_to root_url
   end
 
   # PUT /time_logs/update
   def update
-    current_time_log = TimeLog.find(params[:time_log_id])
+    current_time_log = current_user.current_time_log
 
     if params[:in_out] == 'in'
       current_time_log.update_attributes in: params[:time]
