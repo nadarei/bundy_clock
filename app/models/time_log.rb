946f12585eb6cd04c188cf252c64acede2921dcb
Benjamin Oris Jr <bcoris@gmail.com>
2012-03-01 17:27:51 +0800

    Create comment_text method in time_log model.

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -11,4 +11,13 @@ class TimeLog < ActiveRecord::Base
       (out - self.in) / 60 / 60
     end
   end
+
+  def comment_text=(value)
+    if comment
+      comment.update_attributes comments: value
+    else
+      c = Comment.create time_log: self, comments: value
+      self.comment = c
+    end
+  end
 end
