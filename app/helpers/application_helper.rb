23fc95970d44551178fc35ec5b67003ee0fbef74
mikong <michaelgalero@gmail.com>
2012-03-07 18:36:40 +0800

    Use Rails-way of setting date and time formats.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,19 +2,7 @@ module ApplicationHelper
 
   def short_time(time)
     # config.time_zone set to 'Beijing'. No 'Manila' yet in Rails. Boo!
-    time.in_time_zone.strftime("%H:%M")
-  end
-
-  def just_date(time)
-    time.in_time_zone.strftime("%B %e, %Y")
-  end
-
-  def just_day(date)
-    date.strftime("%a, %e")
-  end
-
-  def month_year(month)
-    month.strftime("%B %Y")
+    time.in_time_zone.to_s(:time)
   end
 
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Date::DATE_FORMATS[:month_year] = "%B %Y"
+Date::DATE_FORMATS[:daily] = "%a, %e"
