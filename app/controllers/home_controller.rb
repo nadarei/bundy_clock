05cc08f6ad7da4156ce6decbdfdd5b255cd7f48e
Benjamin Oris Jr <bcoris@gmail.com>
2012-03-09 19:01:54 +0800

    Move month and archive actions to time_logs controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,24 +4,9 @@ class HomeController < ApplicationController
   def index
     if logged_in?
       @users = User.order('name')
-      render action: 'index'
+      render 'index'
     else
-      render action: 'home'
+      render 'home'
     end
-  end
-
-  def month
-    @date = params[:date].nil? ? Date.today : Date.parse(params[:date])
-    @year = @date.year
-    @month = @date.month
-    @days = (1..(Date.civil(@year, @month, -1).day))
-    @dates = @days.map { |d| Date.parse "#{@year}-#{@month}-#{d}" }
-    @users = User.order('name')
-  end
-
-  def archive
-    @users = User.order('name')
-    today = Date.today
-    @dates = [ today.prev_month, today, today.next_month ]
   end
 end
--- app/controllers/time_logs_controller.rb
+++ app/controllers/time_logs_controller.rb
@@ -40,4 +40,19 @@ class TimeLogsController < ApplicationController
     redirect_to :back
   end
 
+  def month
+    @date = params[:date].nil? ? Date.today : Date.parse(params[:date])
+    @year = @date.year
+    @month = @date.month
+    @days = (1..(Date.civil(@year, @month, -1).day))
+    @dates = @days.map { |d| Date.parse "#{@year}-#{@month}-#{d}" }
+    @users = User.order('name')
+  end
+
+  def archive
+    @users = User.order('name')
+    today = Date.today
+    @dates = [ today.prev_month, today, today.next_month ]
+  end
+
 end
