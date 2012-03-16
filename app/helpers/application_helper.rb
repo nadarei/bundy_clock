c05a8b98834df0b54cbf0968144b00ddc4402915
Benjamin Oris Jr <bcoris@gmail.com>
2012-03-16 19:27:37 +0800

    Use date and user id as table row id.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,10 @@ module ApplicationHelper
     time.in_time_zone.to_s(:time)
   end
 
+  # Returns the HTML ID for the <tr> for a time log.
+  def row_id(date, user)
+    datestr = date.strftime("%Y%m%d")
+    "log-#{datestr}-#{user.id}"
+  end
+
 end
