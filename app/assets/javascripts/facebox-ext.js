cb8583780133ef3b343542d5b8403966df0b7e1f
Benjamin Oris Jr <bcoris@gmail.com>
2012-03-22 18:22:33 +0800

    Configure Rails to use Facebox.

---
+++ app/assets/javascripts/facebox-ext.js
@@ -0,0 +1,10 @@
+jQuery.facebox.settings.closeImage = '/assets/closelabel.png';
+jQuery.facebox.settings.loadingImage = '/assets/loading.gif';
+jQuery.facebox.settings.faceboxHtml  = '\
+    <div id="facebox" style="display:none;"> \
+      <div class="popup"> \
+        <div class="content"> \
+        </div> \
+        <a href="#" class="close"><img src="/assets/closelabel.png" title="close" class="close_image" /></a> \
+      </div> \
+    </div>';
