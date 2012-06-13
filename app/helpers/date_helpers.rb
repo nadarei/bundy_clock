575c340888fa39bf80c89683502a0a1d83cbbc40
Benjamin Oris Jr <bcoris@gmail.com>
2012-06-13 17:23:56 +0800

    Show more months in archive.

--- app/helpers/date_helpers.rb
+++ app/helpers/date_helpers.rb
@@ -1,4 +1,5 @@
 module DateHelpers
+  include Calendar
   # Returns a range with the start and end of the month that the given date is
   # in.
   #
@@ -12,6 +13,17 @@ module DateHelpers
     end_date = today  if date.end_of_month > today
 
     (date.beginning_of_month..end_date)
+  end
+
+  def range_of_months
+    year_first_deployed = 2012
+    starting_month = today.year == year_first_deployed ? 2 : 1
+    ending_month = today.month
+
+    (starting_month..ending_month).to_a.map do |m|
+      datestr = Date::MONTHNAMES[m] + " " + today.year.to_s
+      Date.parse(datestr)
+    end
   end
 
   def today
